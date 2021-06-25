81b4798fd4abaf9bb10a52d599c3129f8aede5cc
bendunhampdx <bendunhampdx@gmail.com>
2021-06-25 09:38:57 -0700

    write first test for person01 class

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,8 @@
+import {Person01} from '../src/js/calculator.js'
+
+describe('Person01', () => {
+  test('should correctly create a person class object with age property', () => {
+    let newPerson = new Person01(75);
+    expect(newPerson.age).toBeDefined();
+  })
+})
