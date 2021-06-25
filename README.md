8f84907b4c34e5a004348a31024559d5c6876134
bendunhampdx <bendunhampdx@gmail.com>
2021-06-25 15:59:31 -0700

    update README.md

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
-# (Application Name)
+# (Galactic Age Calculator)
 
-#### (Brief Description of Application)
+#### (The Galactic Age Calculator will tell you your age and life expectency on Mars, Venus, Mercury, and Jupiter)
 
-#### By (Your Name Here)
+#### By (Ben Dunham)
 
 ## Technologies Used
 
-* _List all_
-* _the major technologies_
-* _you used in your project_
-* _here_
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _Babel_
+* _eslintl_
+* _Jest Testing_
 
 ## Description
-
+* _This application will provide the business logic for an application meant to allow a user to enter their age and life expectency and discover the equivilents of those properties on the planets Mercury, Venus, Mars, and Jupiter._
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
+
+## Set Up Requirements
+* _clone project to your desktop_
+* _in terminal run : get clone https://github.com/bendunhampdx/galactic-calculator.git_
+* _navigate to project by running the command cd galactic-calculator_
+* _run command .code to open in VS Code_
+* _run command npm install to install dependencies_
+* _run command npm start to open page in a live server_
 
 ## Known Bugs
 
-* _Any known issues_
-* _should go here_
+* _No known bugs_
+
 
 ## License
+* _MIT_
 
 ## Contact Information
+* _bendunhampdx@gmail.com_
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -2,7 +2,6 @@ export class Person01 {
   constructor(age, lifeExp) {
     this.age = age
     this.lifeExp = lifeExp
-    this.newMercuryAge = 0
     this.mercuryLifeExp = 0
     this.mercuryLife = 0
     this.venusLifeExp = 0
@@ -22,55 +21,43 @@ export class Person01 {
     return this.newVenusAge;
   };
   marsAge() {
-    this.newMarsAge = this.age * 1.88
+    this.newMarsAge = this.age * 1.88;
     return this.newMarsAge;
-  }
+  };
   jupiterAge() {
-    this.newJupiterAge = this.age * 11.86
-    return this.newjupiterAge
-  }
+    this.newJupiterAge = this.age * 11.86;
+    return this.newjupiterAge;
+  };
   mercuryLifeEx() {
-    this.mercuryLifeExp = this.lifeExp * .24
+    this.mercuryLifeExp = this.lifeExp * .24;
     return this.mercuryLifeExp;
-    }
+    };
   mercuryLifeLeft() {
-    this.mercuryLife = this.mercuryLifeExp - this.newMercuryAge
-    return this.mercuryLife
-  }
+    this.mercuryLife = this.mercuryLifeExp - this.newMercuryAge;
+    return this.mercuryLife;
+  };
   venusLifeEx() {
-    this.venusLifeExp = this.lifeExp * .62
+    this.venusLifeExp = this.lifeExp * .62;
     return this.venusLifeExp;
-    }
+    };
   venusLifeLeft() {
-    this.venusLife = this.venusLifeExp - this.newVenusAge
-    return this.venusLife
-  }
+    this.venusLife = this.venusLifeExp - this.newVenusAge;
+    return this.venusLife;
+  };
   marsLifeEx() {
-    this.marsLifeExp = this.lifeExp * 1.88
+    this.marsLifeExp = this.lifeExp * 1.88;
     return this.marsLifeExp;
-    }
+    };
     marsLifeLeft() {
-      this.marsLife = this.marsLifeExp - this.newMarsAge
-      return this.marsLife
-    }
+      this.marsLife = this.marsLifeExp - this.newMarsAge;
+      return this.marsLife;
+    };
     jupiterLifeEx() {
-      this.jupiterLifeExp = this.lifeExp * 11.86
+      this.jupiterLifeExp = this.lifeExp * 11.86;
       return this.jupiterLifeExp;
-      }
+      };
       jupiterLifeLeft() {
-        this.jupiterLife = this.jupiterLifeExp - this.newJupiterAge
-        return this.jupiterLife
-      }
+        this.jupiterLife = this.jupiterLifeExp - this.newJupiterAge;
+        return this.jupiterLife;
+      };
 }
-
-
-// mercuryLifeEx() {
-//   this.mercuryLifeExp = this.lifeExp * .24
-//   let newMercuryLifeExp = this.mercuryAge - this.mercuryLifeExp
-//   if (newMercuryLifeExp <= 0) {
-//     newMercuryLifeExp = newMercuryLifeExp * -1;
-//   }else {
-//     newMercuryLifeExp = newMercuryLifeExp * -1;
-//   return this.newMercuryLifeExp;
-//   }
-
