0d79d77e012f9081fad6523cd3c78c1b499d31e5
bendunhampdx <bendunhampdx@gmail.com>
2021-06-25 09:46:40 -0700

    create life expectency property

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,5 +4,9 @@ describe('Person01', () => {
   test('should correctly create a person class object with age property', () => {
     let newPerson = new Person01(75);
     expect(newPerson.age).toBeDefined();
-  })
-})
+  });
+  test('should correctly create a person class object with life expectency property', () => {
+    let newPerson = new Person01(75, 120);
+    expect(newPerson.lifeExp).toBeDefined();
+  });
+});
